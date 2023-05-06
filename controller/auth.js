f5458f840846af76bd5555131fff97caa70929e9
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-06 17:17:44 -0300

    logout route

--- controller/auth.js
+++ controller/auth.js
@@ -51,7 +51,12 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.send('logout user');
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ message: 'user logged out!' });
 };
 
 module.exports = {
