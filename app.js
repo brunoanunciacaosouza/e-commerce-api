96ff8a5595953f7368f0c70ba7b8adfb49a042d9
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-05 23:08:17 -0300

    basic express server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
