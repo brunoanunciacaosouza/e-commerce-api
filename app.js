52f1495a99f59f2a31a8d85a84bad79cf8541b6d
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-05 23:24:29 -0300

    basic routes

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const connectDB = require('./db/connect');
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>e-commerce api</h1>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
