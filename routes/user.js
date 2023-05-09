81ef35cc93f8f96da64cdf1b36a81b58d7130127
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-09 20:43:47 -0300

    show me controller

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,7 @@ const {
 router
   .route('/')
   .get(authenticateUser, authorizePermissions('admin'), getAllUsers);
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
 router.route('/:id').get(authenticateUser, getSingleUser);
