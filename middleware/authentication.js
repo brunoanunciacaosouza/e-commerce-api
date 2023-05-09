7995fdb821cf0562a11ef042c1f5cd8a5cb5b60c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-09 20:35:55 -0300

    authorized permissions - complete

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -17,11 +17,15 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-const authorizePermissions = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    throw new CustomErr.UnauthorizedError('Unauthorized to access this route');
-  }
-  next();
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomErr.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next();
+  };
 };
 
 module.exports = { authenticateUser, authorizePermissions };
