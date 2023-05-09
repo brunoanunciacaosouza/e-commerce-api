12e9fcffdd9a5460479214e4d6fe257cd0ac549c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-09 20:21:47 -0300

    authorized permissions - setup

--- routes/user.js
+++ routes/user.js
@@ -10,9 +10,12 @@ const {
   updateUserPassword,
 } = require('../controller/user');
 
-const { authenticateUser } = require('../middleware/authentication');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
 
-router.route('/').get(authenticateUser, getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions, getAllUsers);
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
